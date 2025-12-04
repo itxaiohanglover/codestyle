7d4fd901d5cee5944a46dd79e6b4f165d71aed88
gccszss <2061675285@qq.com>
2025-12-04 21:44:10 +0800

    删除了不必要的本地文档说明，维持仓库的规范

--- codestyle-cloud/codeStyle-remote-search/schema.sql
+++ codestyle-cloud/codeStyle-remote-search/schema.sql
@@ -1,3 +1,8 @@
+-- MySQL建表语句脚本
+-- 生成时间: 2025-12-04
+-- 符合表结构: id BIGINT NOT NULL, meta_json JSON NOT NULL, create_time DATETIME NULL DEFAULT CURRENT_TIMESTAMP, update_time DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, deleted TINYINT NOT NULL DEFAULT 0, group_id VARCHAR(255) GENERATED ALWAYS AS (meta_json->>'$.groupId') VIRTUAL
+-- 注意：group_id是虚拟列，由数据库自动从meta_json中提取，不需要手动插入
+-- 注意：create_time、update_time和deleted字段有默认值，不需要手动插入
 DROP TABLE IF EXISTS tb_remote_meta_info;
 CREATE TABLE tb_remote_meta_info (
     id BIGINT NOT NULL,
